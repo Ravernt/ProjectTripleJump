1e79f40d02e5f63e800dcf92754f6be6071fdbec
gerram1 <gerram1@ktu.lt>
2025-03-24 03:11:50 +0200

    Add files via upload

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("AiridasScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
+
---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ContinueGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; // Freeze game time
+        isPaused = true;
+    }
+
+    public void ContinueGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; // Resume game time
+        isPaused = false;
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
