d82bd4dfb740903d1a6ad4ae520cba9f762f1437
gerram1 <gerram1@ktu.lt>
2025-04-07 04:06:01 +0300

    Update PowerPickup.cs with sound

--- Assets/Scripts/Player/PowerPickup.cs
+++ Assets/Scripts/Player/PowerPickup.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class PowerupPickup : MonoBehaviour
 {
     [SerializeField] private AbilityType abilityToUnlock;
-
+    AudioManager audioManager;
+    
+    void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out PlayerAbilities player))
         {
+            audioManager.PlaySFX(audioManager.collect);
             player.UnlockAbility(abilityToUnlock);
             Destroy(gameObject);
         }
