b1d2b085fec9860baf316a294e6572aedb4770a5
Putes Valdovas <putesvaldovas@gmail.com>
2025-05-12 02:17:29 +0300

    Update Chekpoint.cs

--- Assets/Scripts/Enviroment/Checkpoint.cs
+++ Assets/Scripts/Enviroment/Checkpoint.cs
@@ -3,24 +3,51 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     AudioManager audioManager;
+    SpriteRenderer spriteRenderer;
     [SerializeField] Health health;
-    [SerializeField] private bool canReactivate = false;
-    bool wasChecked = false;
+    //[SerializeField] private bool canReactivate = true;
+    bool isChecked = false;
+    Checkpoint[] checkpoints;
+    Color tempColor;
 
-    void Awake()
+    void Awake()                      
     {
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
         health = FindAnyObjectByType<Health>();
+        checkpoints = FindObjectsByType<Checkpoint>(FindObjectsSortMode.None);
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
-
+    private void Update()
+    {
+        /*
+        if(health.initialPosition.x != transform.position.x || health.initialPosition.y != transform.position.y)
+        {
+            tempColor = spriteRenderer.color;
+            tempColor.a = 0f;
+            spriteRenderer.color = tempColor;
+        }*/
+    }
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.CompareTag("Player") && (!wasChecked || canReactivate))
+        if (collider.CompareTag("Player") && !isChecked)
         {
             audioManager.PlaySFX(audioManager.checkpoint);
-            wasChecked = true;
-            //health.initialPosition = transform.position;
+            //set a new color
+            tempColor.a = 250f;
+            spriteRenderer.color = tempColor;
             health.initialPosition = transform.position;
+            foreach (Checkpoint checkpoint in checkpoints)
+            {
+                if(checkpoint.isChecked)
+                {
+                    //reset to old color, dont forget the checkoint. prefix
+                    checkpoint.tempColor = checkpoint.spriteRenderer.color;
+                    checkpoint.tempColor.a = 0f;
+                    checkpoint.spriteRenderer.color = checkpoint.tempColor;
+                    checkpoint.isChecked = false;
+                }
+            }
+            isChecked = true;
         }
     }
 }
